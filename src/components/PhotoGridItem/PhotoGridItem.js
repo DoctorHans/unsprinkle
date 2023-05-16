cdf7b37d6c5fcac684549516134bf35baaa3714a
DoctorHans <DoctorHans@users.noreply.github.com>
2023-05-16 23:06:02 +0200

    Exercises 1 and 2

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/jpg"
+            srcSet={`
+              ${src} 1x, 
+              ${src.replace(".jpg", "@2x.jpg")} 2x, 
+              ${src.replace(".jpg", "@2x.jpg")} 3x
+              `}
+          />
+          <Image src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,6 +38,7 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
